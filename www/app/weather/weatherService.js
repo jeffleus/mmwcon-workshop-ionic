62c12dadbc6a15e6b434cbb6fc37b45489ced7ad
jeffleus <jeffleus@gmail.com>
2015-09-18 15:46:05 -0700

    additional code comments on the weather svc

--- www/app/weather/weatherService.js
+++ www/app/weather/weatherService.js
@@ -2,10 +2,14 @@ angular.module('starter.services')
 
 .service('WeatherSvc', function($http, $timeout) {
   function _getWeather(zip) {
+    //calling svc w/ $timeout wrapper to allow forced delay for UI testing
     return $timeout(function() {
+        //use the $http svc to make a direct call to API and return the promise
       return $http.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&units=imperial');
-    }, 2500);
+    }, 250);
+      //note, this last parameter can be used to add a svc delay for demo'g the $ionicLoading
   }
+    //wrap the private method in an object and return from the svc singleton
   return {
     getWeather: _getWeather
   };
