ae7d68a7d28bc8f1c65772cbef452c86a0c1ad0b
jeffleus <jeffleus@gmail.com>
2015-09-18 14:05:33 -0700

    commented code in the camera module for service, controller and view

--- www/app/camera/cameraController.js
+++ www/app/camera/cameraController.js
@@ -1,14 +1,24 @@
 angular.module('starter.controllers')
-
+/*
+Injected Services
+*****************
+-timeout used to cancel actionSheet after a delay w/ no interaction
+-cordovaCamera used to control the camera plugin (available thru ngCordova module)
+-ionicActionSheet, used as example of iOS interface for choosing next action
+-ImageSvc, provides the data service for managing a list of images grabbed thru camera
+*/
 .controller('CameraCtrl', function($scope, $timeout, $cordovaCamera, $ionicActionSheet, ImageSvc) {
+    //link the service for images to the scope for binding in the view
     $scope.imageSvc = ImageSvc;
-    $scope.takePicture = _takePicture;
+    //link the private actionSheet method to scope to allow binding to the view button
     $scope.addImageSheet = _addImageSheet;  
 
     function _addImageSheet() {
         var hideSheet = $ionicActionSheet.show({
             buttons: [
+                //button index used to identify which button is clicked
                 { text: 'Take New Photo' },
+                //options included to select from photo library
                 { text: 'Exisiting Photo' }
             ],
             titleText: 'Add Image',
@@ -26,25 +36,34 @@ angular.module('starter.controllers')
             hideSheet();
         }, 3000);
     }
-
+    //private method used to call the camera plugin for taking a picture
     function _takePicture(useLibrary) {
+        console.log('CameraCtrl::takePicture start the camera plugin');
+        //option object used to configure camera when calling the native camers
         var options = {
           quality: 80,
+        //note, this can be changed to return dataURL format instead of files
           destinationType: Camera.DestinationType.FILE_URI,
+        //use the clicked button index to choose camera/library
           sourceType: (useLibrary==0)?Camera.PictureSourceType.CAMERA:Camera.PictureSourceType.PHOTOLIBRARY,
           allowEdit: false,
           encodingType: Camera.EncodingType.JPEG,
           targetWidth: 850,
           targetHeight: 1100,
+        //use to opt out of saving a copy of the image in the library
           saveToPhotoAlbum: false
         };
 
+        //async design: ask for picture --> do something w/ the data
+        // ngCordova uses $promises and .then( function() { ... }) design
         $cordovaCamera.getPicture(options).then(function(imageData) {
+            console.log('CameraCtrl::getPicture end the promise and save the image');
+            //this is where I think the factory class in my service is worth the effort
             var img = new Image();
             img.data = imageData;
             img.title = "test image";
             ImageSvc.addImage(img);
-            
+            //another example of using the $state service for navigation instead of urls
             $state.go('app.camera');
         }, function(err) {
           // error
--- www/app/camera/imageService.js
+++ www/app/camera/imageService.js
@@ -1,24 +1,33 @@
 angular.module('starter.services')
 
+//singleton service to provide a list of image objects
 .service('ImageSvc', function() {
     var self = this;
     //array used for POJO collections
     self.images = [];
+    //publish public methods at the top of the service
     self.addImage = _addImage;
     self.deleteImage = _deleteImage;
-    
+    //private methods backing the public interface methods
     function _addImage(image) {
+        //console logs like this help a lot in the debuggger to see
+        // what is going on w/ your service api
         console.log('ImageSvc::addImage');
+        //poor man's indexing as items are added to the collection
         image.id = self.images.length;
         self.images.push(image);
     }
     
     function _deleteImage(image) {
+        //more console logging
         console.log('ImageSvc::deleteImage');
+        //check for the existence of the object in the collection array
         var index = self.images.indexOf(image);
         if (index >-1) {
+            //if found, splice out the item at the index
             self.images.splice(index,1);
         } else {
+            //otherwise, log to the console that now image found
             console.log('image not found in ImageSvc');
         }
     }
